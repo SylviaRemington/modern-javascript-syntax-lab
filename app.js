c4272cb92cc9de6c854d36c2b7137a9c9bb52198
SylviaRemington <sylviaremington@gmail.com>
2025-07-14 11:36:21 -0700

    exercise 1 completed

---
+++ app.js
@@ -0,0 +1,37 @@
+// Exercise 1 - Applying map()
+// Assignment info is as follows:
+// a. Use the `map()` method to iterate over the provided `nums` array
+//
+// b. Use the callback function of the `map()` method you wrote to
+//    create a new array where each value is multiplied by 2.
+//    Name the new array `numsTimesTwo`.
+//
+// c. Console log the new array.
+//
+// Starting code (don't modify this):
+// const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+const numsTimesTwo = nums.map((num) => {
+    return num*2;
+});
+
+console.log(numsTimesTwo);
+
+// ______________________________________________________________
+
+
+
+
+
+
+
+
+
+
+// ______________________________________________________________
+
+
+
+
