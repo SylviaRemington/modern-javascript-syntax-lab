06f0ddfcb908469755f5ea395f2ab136e4c91078
SylviaRemington <sylviaremington@gmail.com>
2025-07-14 13:34:48 -0700

    updating importing & exporting js & testing with console log

--- importingFile.js
+++ importingFile.js
@@ -1,4 +1,10 @@
-import name from './exportingFile.js';
-
-console.log(name);
+// import name from './exportingFile.js';
+// console.log(name);
 // Prints: Matt when do this in terminal: node importingFile.js
+
+import name, { computer, age, job } from './exportingFile.js';
+
+console.log(name, computer, age, job);
+// Prints: 'Matt', 'MacBook Pro', 43, 'programmer'
+// node importingFile.js prints: Matt MacBook Pro 43 programmer
+
